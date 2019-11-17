d975d9e8967e539fb7f99626c77916fccb841f93
GamingJam <GamingJamTv@gmail.com>
2019-11-17 16:49:02 +0300

    add random data generation

---
+++ src/test/JMain.java
@@ -0,0 +1,20 @@
+package test;
+import java.io.FileNotFoundException;
+
+public class JMain {
+    public static void main(String[] args) throws FileNotFoundException {
+        Generator g = new Generator();
+        System.out.println(g.getRandomDate());
+        System.out.println(g.getFemaleName() + " " + g.getSurname());
+        System.out.println(g.getMaleName() + " " + g.getSurname());
+        System.out.println(g.getBool());
+        System.out.println(g.getMedicalFact());
+        System.out.println(g.getMedicalFact());
+        System.out.println(g.getMedicalFact());
+        System.out.println(g.getMedicament());
+        System.out.println(g.getMedicament());
+        System.out.println(g.getMedicament());
+
+
+    }
+}
