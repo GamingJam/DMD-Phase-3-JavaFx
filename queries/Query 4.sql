13d0fb426898547fd5c4c196a29dee5b6531eac1
supersloy <51485198+supersloy@users.noreply.github.com>
2019-11-21 19:15:09 +0300

    Add files via upload

---
+++ queries/Query 4.sql
@@ -0,0 +1,36 @@
+create table possible_charge(
+	charge int
+);
+INSERT INTO possible_charge VALUES (200);
+INSERT INTO possible_charge VALUES (250);
+INSERT INTO possible_charge VALUES (400);
+INSERT INTO possible_charge VALUES (500);
+
+SELECT SUM(charge) as income_in_rubles
+FROM (
+         SELECT age, amount_of_appointments
+         FROM (
+                  SELECT id as patient_id, age
+                  FROM patient
+                           JOIN "user" on patient.user_ssn = "user".ssn
+              ) as age_of_patient
+                  JOIN
+              (
+                  SELECT for_patient_id, count(app_id) as amount_of_appointments
+                  FROM (
+                           SELECT id as app_id, for_patient_id
+                           FROM appointment
+                           WHERE (EXTRACT(year FROM date) = EXTRACT(year FROM current_date))
+                             AND (EXTRACT(month FROM date) = EXTRACT(month FROM current_date) - 1)
+                       ) as appoinments_in_previous_month
+                  GROUP BY for_patient_id
+              ) as amount_of_appoinments_in_previous_month
+              on patient_id = for_patient_id
+     ) as age_and_amount_of_appointments_in_previous_month_of_patient
+         CROSS JOIN possible_charge
+WHERE (age < 50 AND amount_of_appointments < 3 AND charge = 200)
+   OR (age < 50 AND amount_of_appointments >= 3 AND charge = 250)
+   OR (age >= 50 AND amount_of_appointments < 3 AND charge = 400)
+   OR (age >= 50 AND amount_of_appointments >= 3 AND charge = 500);
+
+DROP table possible_charge;
---
+++ queries/Query 5.sql
@@ -0,0 +1,38 @@
+--delete distinct from inner selects if they are not unique
+SELECT ssn, first_name, last_name
+FROM(
+    SELECT ssn, first_name, last_name
+    FROM(
+        SELECT ssn, first_name, last_name, count(year) as amount_of_hard_working_years
+        FROM(
+            SELECT ssn, first_name, last_name, year
+            FROM(
+                SELECT ssn, first_name, last_name, year, count(for_patient_id) as patients_in_year
+                FROM(
+                    SELECT distinct ssn, first_name, last_name, EXTRACT(year from date) as year, for_patient_id
+                    FROM "user" join appointment on appointed_by_user_ssn = ssn
+                    WHERE role = 'doctor' AND EXTRACT(year from date) > EXTRACT(year from CURRENT_DATE) - 10
+                ) as patients_visited_by_doctor_in_year
+                GROUP BY ssn, first_name, last_name, year
+            ) as amount_of_patient_visited_by_doctor_in_year
+            WHERE patients_in_year >= 1
+        ) as hard_working_years_of_doctor
+        GROUP BY ssn, last_name, first_name
+    ) as amount_of_ard_working_years_of_doctor
+    WHERE amount_of_hard_working_years = 2
+) as doctors_with_not_less_than_5_patients_per_year 
+JOIN 
+(
+    SELECT ssn as ssn_of_doctor_with_100_patients
+    FROM(
+        SELECT ssn, count(for_patient_id) as total_patients
+        FROM(
+            SELECT distinct ssn, for_patient_id
+            FROM "user" join appointment on appointed_by_user_ssn = ssn
+            WHERE role = 'doctor' AND EXTRACT(year from date) > EXTRACT(year from CURRENT_DATE) - 10
+        ) as patients_visited_by_doctor_ssn
+        GROUP BY ssn
+    ) as amount_of_patients_visited_by_doctor_ssn
+    WHERE total_patients >= 1
+) as doctors_with_not_less_than_100_patients 
+on ssn = ssn_of_doctor_with_100_patients
