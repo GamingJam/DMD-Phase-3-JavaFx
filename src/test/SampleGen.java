703e36ee4aba908708771e19c43f498ab1c52a7a
Mike <super.mvk@yandex.ru>
2019-11-26 21:11:53 +0300

    Prompt message for generate script

--- src/test/SampleGen.java
+++ src/test/SampleGen.java
@@ -4,13 +4,21 @@ import test.Generation.DataGenerator;
 import test.Generation.PostgresGen;
 
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class SampleGen {
     public static void main(String[] args) throws FileNotFoundException {
         PostgresGen gen = new PostgresGen();
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please, prompt sql file name to generate: ");
+        String filename = scan.nextLine();
+        if(filename.equals("")){
+            filename = "sql/postgres_insert.sql";
+        }
+
         System.out.print("Generation: ");
-        gen.generateToFile("sql/postgres_insert.sql");
+        gen.generateToFile(filename);
         System.out.println("done!");
 
     }
